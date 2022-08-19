90eb393a11793b994f75c7a2b3d6eee8de1174a5
anthony <anthony@win11>
2022-08-19 15:41:31 -0500

    Remove some boilerplate text

--- pages/index.js
+++ pages/index.js
@@ -25,8 +25,7 @@ export default function Home({ allPostsData }) {
       <section className={utilStyles.headingMd}>
         <p>Introduction</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          Hello!
         </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
