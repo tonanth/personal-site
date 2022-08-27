bee652670ebc93d6826409faf35f4158a601c2f6
anthony <anthony@win11>
2022-08-26 20:48:26 -0500

    Rename from mjs to js to do middleware

---
+++ next.config.js
@@ -0,0 +1,18 @@
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+    // If you use `MDXProvider`, uncomment the following line.
+    providerImportSource: "@mdx-js/react",
+  },
+})
+
+module.exports = withMDX({
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+  experimental : {
+    images: {
+      unoptimized: true,
+    },
+  }
+});
