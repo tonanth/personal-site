b9ff59d63de113f04c518c05f82b38c3dca23fb5
anthony <anthony@win11>
2022-09-02 18:45:05 -0500

    Add github link and intro

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,6 @@ import { getSortedPostsData } from '../lib/posts';
 import Link from 'next/link';
 import Date from '../components/date';
 
-
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
   return {
@@ -23,9 +22,9 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Introduction</p>
+        <p><a href='github.com/tonanth'></a></p>
         <p>
-          Hello!
+          Hello! I'm a software engineer.
         </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
