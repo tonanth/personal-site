d223ef18c9bcae148204e37116119d5d71bdd612
anthony <anthony@win11>
2022-09-07 00:02:34 -0500

    Add syntax highlighting to react-markdown

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,3 +1,5 @@
+// @ts-nochecks
+
 import Layout from '../../components/layout';
 import { getAllPostIds, getPostData } from '../../lib/posts';
 import Head from 'next/head';
@@ -6,7 +8,8 @@ import utilStyles from '../../styles/utils.module.css';
 import styles from '../../styles/article.module.scss'
 import ReactMarkdown from 'react-markdown';
 
-
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { darcula } from 'react-syntax-highlighter/dist/cjs/styles/prism'
 
 export async function getStaticProps({ params }) {
   const postData = await getPostData(params.id);
@@ -41,7 +44,42 @@ export default function Post({ postData }) {
             {postData.description}
           </div>
         </div>
-        <ReactMarkdown className={styles.articlebody} children={postData.contentMarkdown} />
+
+        
+        <ReactMarkdown className={styles.articlebody} children={postData.contentMarkdown} 
+        components = {{
+          code({node, inline, className, children, ...props}) {
+            if (inline) {
+              return (
+                <code>{children}</code> 
+              )
+            } else {
+              const match = /language-(\w+)/.exec(className || '');
+              const lang = match ? match[1] : 'none';
+              console.log(lang)
+              return (
+                <SyntaxHighlighter  
+                  children={String(children).replace(/\n$/, '')}
+                  language={lang}
+                  codeTagProps={{style: {fontFamily: 'Fira Code'}}}
+                />
+              )
+            }
+            // return !inline && null ? (
+            //   <SyntaxHighlighter
+            //     children={String(children).replace(/\n$/, '')}
+            //     style={dark}
+            //     language={null}
+            //     PreTag="div"
+            //     {...props}
+            //   />
+            // ) : (
+            //   <code className={className} {...props}>
+            //     {children}
+            //   </code>
+            // )
+          }
+        }} />
         {/* <div className={styles.articlebody} dangerouslySetInnerHTML={{ __html: postData.contentHtml }} /> */}
       </article>
     </Layout>
